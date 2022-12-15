c77f82c734a6210fa7d8691215a5475ae386a653
woke02 <justwjyk@gmail.com>
2022-12-15 21:11:18 +0800

    WhatsApp-Clone

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import '../App.css'
+import Login from './Login'
+import useLocalStorage from '../hooks/useLocalStorage'
+import Dashboard from './Dashboard'
+import { ContactsProvider } from '../contexts/ContactsProvider'
+import { ConversationsProvider } from '../contexts/ConversationsProvider'
+import { SocketProvider } from '../contexts/SocketProvider'
+
+
+function App() {
+  const [id, setId] = useLocalStorage('id')
+
+  const dashboard = (
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
+  )
+
+  return (
+    id ? dashboard : <Login onIdSubmit={setId} />
+  )
+}
+
+export default App;
